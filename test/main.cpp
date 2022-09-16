a5696085d1ab31bd19f239f1998e9255e5a1be0a
孙笑川 <94502244+PanzerVI-Tiger@users.noreply.github.com>
2022-09-16 22:28:10 +0800

    update

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,22 @@
 #include "../head/type_traits.hpp"
 
+#include <corecrt.h>
 #include <iostream>
 
+struct eat_any {
+    template<typename Type>
+    eat_any(const Type&) throw()
+    {}
+};
+
+template<size_t size>
+void f(const eat_any(&)[size]) throw()
+{
+    std::cout << size << "\n";
+}
 
 int main(int, char**) {
-    int i[mylib::integral_sequence<int, 10>::value];
+    typedef int type[10];
+    type(4);
+    f({ 1, 2, 3 });
 }
