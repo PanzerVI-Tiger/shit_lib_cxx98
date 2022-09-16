c2471b8f5b4be0ee3bdd9f4725bda9829dd72cf2
孙笑川 <94502244+PanzerVI-Tiger@users.noreply.github.com>
2022-09-16 22:29:27 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/PanzerVI-Tiger/Documents/vscode/shit_lib_cxx98
-# Build directory: C:/Users/PanzerVI-Tiger/Documents/vscode/shit_lib_cxx98/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
